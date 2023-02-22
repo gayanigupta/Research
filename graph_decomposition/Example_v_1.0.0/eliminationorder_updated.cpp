7fd6da8f37846519c1a24ae741a20585021199cd
Gayani Gupta <gayani.gupta@gmail.com>
2023-02-22 12:03:19 -0600

    tree decomposition code updated
    tree decomposition code updated

--- graph_decomposition/Example_v_1.0.0/eliminationorder_updated.cpp
+++ graph_decomposition/Example_v_1.0.0/eliminationorder_updated.cpp
@@ -1,25 +1,24 @@
 // C++ program to find K-Cores of a graph
 #include <iostream>
-#include<list>
+#include <list>
 
-//INPUT HEADERS
+// INPUT HEADERS
 #include "translate_from_input.hpp"
 #include "input_to_network.hpp"
-#include"structure_defs.hpp"
+#include "structure_defs.hpp"
 #include "find_neighbors.hpp"
 
-//OUTPUT HEADERS
+// OUTPUT HEADERS
 #include "printout_network.hpp"
 #include "printout_others.hpp"
 using namespace std;
 
-
-int get_max_no_of_network(A_Network& x)
+int get_max_no_of_network(A_Network &x)
 {
 	int max_no = -1;
 	for (int i = 0; i < x.size(); ++i)
 	{
-		vector <int_double>& listW = x[i].ListW;
+		vector<int_double> &listW = x[i].ListW;
 		for (int j = 0; j < listW.size(); ++j)
 		{
 			if (listW[j].first > max_no)
@@ -29,9 +28,9 @@ int get_max_no_of_network(A_Network& x)
 	return max_no;
 }
 
-bool checkEdge(ADJ_Bundle& bundle, int vertex_no)
+bool checkEdge(ADJ_Bundle &bundle, int vertex_no)
 {
-	vector<int_double>& adj = bundle.ListW;
+	vector<int_double> &adj = bundle.ListW;
 	for (int i = 0; i < adj.size(); ++i)
 	{
 		if (adj[i].first == vertex_no)
@@ -41,10 +40,10 @@ bool checkEdge(ADJ_Bundle& bundle, int vertex_no)
 	return false;
 }
 
-bool addUndirectedEdge(ADJ_Bundle& bundle, int vertex_no)
+bool addUndirectedEdge(ADJ_Bundle &bundle, int vertex_no)
 {
 	bool found = false;
-	vector<int_double>& adj = bundle.ListW;
+	vector<int_double> &adj = bundle.ListW;
 	vector<int_double>::iterator it = adj.begin();
 	for (; it != adj.end(); ++it)
 	{
@@ -66,12 +65,12 @@ bool addUndirectedEdge(ADJ_Bundle& bundle, int vertex_no)
 	return false;
 }
 
-bool removeDirectedEdge(A_Network& x, int start_no, int end_no)
+bool removeDirectedEdge(A_Network &x, int start_no, int end_no)
 {
 	if (start_no > x.size() - 1)
 		return false;
 
-	vector<int_double>& listW = x[start_no].ListW;
+	vector<int_double> &listW = x[start_no].ListW;
 	vector<int_double>::iterator it = listW.begin();
 	for (; it != listW.end(); ++it)
 	{
@@ -84,12 +83,12 @@ bool removeDirectedEdge(A_Network& x, int start_no, int end_no)
 	return false;
 }
 
-bool removeUndirectedEdge(A_Network& x, int start_no, int end_no)
+bool removeUndirectedEdge(A_Network &x, int start_no, int end_no)
 {
 	return removeDirectedEdge(x, start_no, end_no) && removeDirectedEdge(x, end_no, start_no);
 }
 
-void convertDirected2UndirectedNetwork(A_Network& a, A_Network& b)
+void convertDirected2UndirectedNetwork(A_Network &a, A_Network &b)
 {
 	int count = get_max_no_of_network(a) + 1;
 	int i = 0;
@@ -105,18 +104,17 @@ void convertDirected2UndirectedNetwork(A_Network& a, A_Network& b)
 
 	for (i = 0; i < a.size(); ++i)
 	{
-		vector<int_double>& adj = a[i].ListW;
+		vector<int_double> &adj = a[i].ListW;
 		for (int j = 0; j < adj.size(); ++j)
 			addUndirectedEdge(b[adj[j].first], i);
 	}
-
 }
 
-void runStandardEliminationOrdering(A_Network& src, vector<int>& orderList, vector<vector<int> >& tree)
+void runStandardEliminationOrdering(A_Network &src, vector<int> &orderList, vector<vector<int> > &tree)
 {
 	A_Network x;
 	convertDirected2UndirectedNetwork(src, x);
-	//print_network(x, "Undirected Network");
+	// print_network(x, "Undirected Network");
 
 	int count = x.size();
 	// Store degrees of all vertices
@@ -166,19 +164,18 @@ void runStandardEliminationOrdering(A_Network& src, vector<int>& orderList, vect
 		processed[sel_vertex] = true;
 		orderList.push_back(sel_vertex);
 
-		tree.push_back(treeVertices);		
-		
-		//print_network(x, "Undirected Network");
+		tree.push_back(treeVertices);
+
+		// print_network(x, "Undirected Network");
 
 		sel_vertex = 0;
 	}
 }
 
-
 // Driver program to test methods of graph class
-int main()
+int main(int argc, char *argv[])
 {
-	vector<Edge> edges;
+	/*vector<Edge> edges;
 	edges.push_back(create(0, 1, 1));
 	edges.push_back(create(0, 2, 1));
 	edges.push_back(create(1, 2, 1));
@@ -195,11 +192,79 @@ int main()
 	edges.push_back(create(5, 6, 1));
 	edges.push_back(create(5, 8, 1));
 	edges.push_back(create(6, 7, 1));
-	edges.push_back(create(6, 8, 1));
+	edges.push_back(create(6, 8, 1));*/
 
+	clock_t q, q1, q2, t;
+	vector<Edge> edges;
+
+	// read edges from file.
+	/*fstream fin;
+	fin.open("Tests/core_2.txt", ios::in);
+	if (fin.is_open() == false)
+		{return 0;}
+
+		cout << "read file went through "<<endl;
+
+	while (!fin.eof())
+	{
+		int start_no, end_no, weight;
+		fin >> start_no >> end_no >> weight;
+		edges.push_back(create(start_no, end_no, 1));
+	}
+
+	fin.close();
+	cout << "read file "<<endl; */
+
+	/*
+
+	// Create a network with edges.
 	A_Network x1;
 	create_Network(&edges, 0, &x1, -1);
+	cout << "**** \n";
+	*/
 
+	// Preprocess Nodes to Numbers
+	// Stores file in argv[3]: store map in argv[4]
+	// Vertices start from 0
+	q = clock();
+	// Check if valid input is given
+	if (argc < 3)
+	{
+		cout << "INPUT ERROR:: At least 2 inputs required. First: filename \n Second: Filetypes: 1:node_node_wt 2:node_wt_node 3:node_node 4:node_node (Only option 1 is active now) \n Third. Name of new file \n Fourth. Name of Map file\n";
+		return 0;
+	}
+	// Check to see if file opening succeeded
+	ifstream the_file(argv[1]);
+	if (!the_file.is_open())
+	{
+		cout << "INPUT ERROR:: Could not open file\n";
+	}
+
+	A_Network x1;
+	int nodes = -1;
+	map_int_st revmap;
+	int type = atoi("1");
+	translate_input(argv[1], type, argv[3], argv[4]);
+
+	// Remove Duplicate Edges and Self Loops; Create Undirected Graphs
+	//  process_to_simple_undirected();
+	q = clock() - q;
+	cout << "Total Time for Preprocessing" << ((float)q) / CLOCKS_PER_SEC << "\n";
+
+	/***** Preprocessing to Graph (GUI) ***********/
+
+	/******* Read Graph (GUI) and Create Reverse Map*****************/
+	// Obtain the list of edges.
+	q = clock();
+	readin_network(&x1, argv[3], nodes);
+
+	// Create Reversemap
+
+	nodes = x1.size();
+	create_map(argv[4], &revmap);
+
+	q = clock() - q;
+	cout << "Total Time for Reading Network" << ((float)q) / CLOCKS_PER_SEC << "\n";
 	vector<int> orderList;
 	vector<vector<int> > tree;
 	runStandardEliminationOrdering(x1, orderList, tree);
@@ -217,7 +282,5 @@ int main()
 		cout << endl;
 	}
 
-
 	return 0;
 }
-
